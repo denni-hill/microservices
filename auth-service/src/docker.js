eb2ab4fba32830a4bd02818dd69182c79a8820f8
Denis Saenko <denissaenkoo@gmail.com>
2022-05-13 03:21:32 +0300

    docker.js fix

--- auth-service/src/docker.js
+++ auth-service/src/docker.js
@@ -1,14 +1,11 @@
 const dotenv = require("dotenv");
 const path = require("path");
+
+dotenv.config();
+dotenv.config({
+  path: path.join(process.cwd(), "..", "services.env", ".env")
+});
+
 const app = require("./app");
-
-if (process.env.NODE_ENV === "docker") {
-  dotenv.config();
-  dotenv.config({
-    path: path.join(process.cwd(), "..", "services.env", ".env")
-  });
-}
-
-app = require("./app");
 
 app.start();
