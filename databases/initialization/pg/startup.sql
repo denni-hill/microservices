4e3c95266c0eec36234e767db8e1cbeddf6f710c
Denis Saenko <denissaenkoo@gmail.com>
2022-07-19 15:27:11 +0300

    counter service integrated in api

--- databases/initialization/pg/startup.sql
+++ databases/initialization/pg/startup.sql
@@ -7,3 +7,8 @@ CREATE USER auth_service_user WITH PASSWORD 'AuthUserPassword';
 CREATE DATABASE auth_database;
 GRANT ALL PRIVILEGES ON DATABASE auth_database TO auth_service_user;
 GRANT ALL PRIVILEGES ON DATABASE auth_database TO postgres;
+
+CREATE USER counter_service_user WITH PASSWORD 'CounterUserPassword';
+CREATE DATABASE counter_database;
+GRANT ALL PRIVILEGES ON DATABASE counter_database TO counter_service_user;
+GRANT ALL PRIVILEGES ON DATABASE counter_database TO postgres;
