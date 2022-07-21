40ac31966e862d7214e3acacea213faf2d89e950
Denis Saenko <denissaenkoo@gmail.com>
2022-07-21 03:36:52 +0300

    error handling for counter-service added

--- counter-service/src/app.ts
+++ counter-service/src/app.ts
@@ -1,16 +1,29 @@
 import sourceMapSupport from "source-map-support";
 import dotenv from "dotenv";
 import path from "path";
-import express from "express";
+import express, { Response } from "express";
 import redisClient from "./redis";
 import "reflect-metadata";
 import { defaultDataSource } from "./database";
+import router from "./router";
+import BaseError from "./errors/base.error";
+import InternalServerError from "./errors/internal.error";
 
 sourceMapSupport.install();
 dotenv.config();
 dotenv.config({ path: path.join(process.cwd(), "..", "services.env", ".env") });
 
 const app = express();
+
+app.use(router);
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((e, _req, res: Response, next) => {
+  if (e instanceof BaseError) {
+    if (e instanceof InternalServerError) console.log(e.error);
+    else res.status(e.getStatusCode()).json(e.getResponseBody());
+  } else console.log(e);
+});
 
 app.start = async () => {
   await defaultDataSource.initialize();
