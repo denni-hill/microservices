2e131ae8bf04e9121cd7489be63c1de68bfb2071
Denis Saenko <denissaenkoo@gmail.com>
2022-09-30 03:29:31 +0300

    swagger is available only in development

--- blog-service/src/main.ts
+++ blog-service/src/main.ts
@@ -5,9 +5,11 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const options = new DocumentBuilder().setTitle("Saentist Blog Api").build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup("/swagger", app, document);
+  if (process.env.NODE_ENV === "development") {
+    const options = new DocumentBuilder().setTitle("Saentist Blog Api").build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup("/swagger", app, document);
+  }
 
   await app.listen(8085);
 }
